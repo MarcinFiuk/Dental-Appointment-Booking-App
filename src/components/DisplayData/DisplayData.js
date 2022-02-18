5df5e4bbf2c44f3324cb8a345c788a3c07f1bee5
Marcin_Fi <fiukmarcinn@gmail.com>
2022-02-18 12:11:57 +0000

    add InteractiveButton

--- src/components/DisplayData/DisplayData.js
+++ src/components/DisplayData/DisplayData.js
@@ -1,33 +1,52 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import useFetchData from '../../hooks/useFetchData';
+import styled from 'styled-components';
 
-const DisplayData = (/*{ profession }*/) => {
+import useFetchData from '../../hooks/useFetchData';
+import { Flex } from './../../Style/Flex';
+import InteractiveButton from '../../Style/InteractiveButton';
+
+const DisplayData = () => {
     const location = useLocation();
     const data = useFetchData('specialists', location.state);
 
     return (
-        <div>
+        <Flex gap='1.5rem' style={{ 'margin-block': '2rem' }}>
             {data.map(({ id, name, surname, availability }) => {
                 return (
-                    <div key={id}>
-                        <h3>{name}</h3>
-                        <h3>{surname}</h3>
-
+                    <Flex key={id}>
+                        <StyledName>
+                            {name} {surname}
+                        </StyledName>
+                        <InteractiveButton>check CV</InteractiveButton>
                         {availability.workAt.map(({ day, hour }) => {
                             return (
                                 <div key={day}>
                                     <p>
                                         {day}: <span>{hour}</span>
+                                        <InteractiveButton>
+                                            book slot
+                                        </InteractiveButton>
                                     </p>
                                 </div>
                             );
                         })}
-                    </div>
+                    </Flex>
                 );
             })}
-        </div>
+        </Flex>
     );
 };
+
+const StyledName = styled.h3`
+    --font-min: 15;
+    --font-max: 30;
+    font-size: calc(
+        ((var(--font-min) / 16) * 1rem) + (var(--font-max) - var(--font-min)) *
+            var(--fluid-bp)
+    );
+    color: var(--mistyBlueLight);
+    -webkit-text-stroke: 1px black;
+`;
 
 export default DisplayData;
